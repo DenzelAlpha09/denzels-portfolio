d7b9f18f61de0a1248ab59d7f974f2f0e5f886aa
Denzel Ashitey <denzelniiashitey@gmail.com>
2025-06-29 09:59:10 +0000

    added separator

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Separator from "./components/Separator"
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Separator />
     </>
     
   )
---
+++ src/components/Separator.jsx
@@ -0,0 +1,10 @@
+export default function Separator() {
+    return (
+        <div className="flex items-center gap-4 justify-center">
+            <div className="h-1 w-8 bg-slate-300"></div>
+            <div className="h-4 w-4 rounded-full bg-slate-300"></div>
+            <div className="h-1 w-8 bg-slate-300"></div>
+            <div></div>
+        </div>
+    )
+}
