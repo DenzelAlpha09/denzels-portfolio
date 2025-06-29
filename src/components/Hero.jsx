5c118d573a973aaea4faf3e7f3b27163d18cbceb
Denzel Ashitey <denzelniiashitey@gmail.com>
2025-06-29 10:57:23 +0000

    added smaller fonts on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ export default function Hero() {
         <section>
             <img className='w-3/4 mx-auto rounded-full border-5 border-[#3A4750]' src={photo}/>
             <div className='flex flex-col py-6 gap-[6px]'>
-                <h2 className='font-medium text-2xl text-[#303841]'>Web and Mobile Developer</h2>
-                <span className='flex gap-2 text-[#3A4750]'><i className="ri-map-pin-3-line"></i> Accra, Ghana</span>
-                <p className='text-[#3A4750] mb-3 text-[14px]'>Hi, I’m Denzel Ashitey, a web app and mobile app developer. I build production ready apps, websites and mobile applications.</p>
-                <p className='text-[#3A4750] text-[14px]'>If you want to know more about me, check my <span className='text-[#2185D5] underline'>about page</span></p>
+                <h2 className='font-medium text-xl text-[#303841]'>Web and Mobile Developer</h2>
+                <span className='flex gap-2 text-[#3A4750] text-[12px]'><i className="ri-map-pin-3-line"></i> Accra, Ghana</span>
+                <p className='text-[#3A4750] mb-3 text-[14px] text-[13px]'>Hi, I’m Denzel Ashitey, a web app and mobile app developer. I build production ready apps, websites and mobile applications.</p>
+                <p className='text-[#3A4750] text-[13px]'>If you want to know more about me, check my <span className='text-[#2185D5] underline'>about page</span></p>
             </div>
         </section>
     )
--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -4,16 +4,16 @@ export default function TechStack() {
             <h3 className="text-center text-[#303841]">My Tech Stack</h3>
             <div className="flex items-center justify-between">
                 <span className="flex flex-col items-center gap-[3px]">
-                    <i className="ri-javascript-fill text-[#3A4750] text-4xl"></i>JS
+                    <i className="ri-javascript-fill text-[#3A4750] text-3xl"></i><span className="text-[#3A4750] text-[13px]">JS</span>
                 </span>
                 <span className="flex flex-col items-center gap-[3px]">
-                    <i className="ri-reactjs-line text-[#3A4750] text-4xl"></i>React/native
+                    <i className="ri-reactjs-line text-[#3A4750] text-3xl"></i><span className="text-[#3A4750] text-[13px]">React/native</span>
                 </span>
                 <span className="flex flex-col items-center gap-[3px]">
-                    <i className="ri-tailwind-css-line text-[#3A4750] text-4xl"></i>Tailwind
+                    <i className="ri-tailwind-css-line text-[#3A4750] text-3xl"></i><span className="text-[#3A4750] text-[13px]">Tailwind</span>
                 </span>
                 <span className="flex flex-col items-center gap-[3px]">
-                    <i className="ri-firebase-line text-[#3A4750] text-4xl"></i>Firebase
+                    <i className="ri-firebase-line text-[#3A4750] text-3xl"></i><span className="text-[#3A4750] text-[13px]">Firebase</span>
                 </span>
             </div>
         </div>
